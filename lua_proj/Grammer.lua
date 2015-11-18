66ed8de86c1c30578842f4c7f6d2cb732e752b73
Hyebin <302890512@qq.com>
2015-11-18 15:12:48 +0000

    add lua grammer

---
+++ lua_proj/Grammer.lua
@@ -0,0 +1,30 @@
+--x= 1; y=2
+--x,y = y,x
+--print(x,y)
+--x = 10
+--local i = 1
+--
+--while i<=x do
+--	local x = i*2
+--	print(x)
+--	i = i+1
+--end
+--i = 21
+--if i > 20 then
+--	local x
+--	x = 20
+--	print(x+2)
+--else
+--	print(x)
+--end
+--
+--print(x)
+x = 10;
+f(x)
+{
+	x-1;
+}
+
+for i=10,f(x) do
+	print(i)
+end
--- lua_proj/table.lua
+++ lua_proj/table.lua
@@ -9,10 +9,16 @@
 	-- l = l.next
 -- end
 
-polyline = {color="blue", thickness = 2, npoints = 4,
-			{x = 0, y = 0},
-			{x = -10, y = 0},
-			{x = -10, y = 1},
-			{x = 0, y = 1}
-			}
-print(polyline[1].x, polyline[1].y)
+-- polyline = {color="blue", thickness = 2, npoints = 4,
+			-- {x = 0, y = 0},
+			-- {x = -10, y = 0},
+			-- {x = -10, y = 1},
+			-- {x = 0, y = 1}
+			-- }
+-- print(polyline[1].x, polyline[1].y)
+
+opnames = {["+"] = "add", ["-"] = "sub", ["*"] = "mul", ["/"] = "div"}
+i = 20; s = "-"
+a = {[i+0] = s, [i+1] = s .. s, [i+2] = s..s..s}
+print(opnames[s])
+print(a[21])
